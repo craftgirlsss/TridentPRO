cb74a9e02112262818c4720e8218961f52270d31
Fani Annur Rozi <crazyhx4@gmail.com>
2025-05-23 16:43:39 +0700

    fix: Skip Login if you already have an active token refresh

--- lib/src/views/authentications/splashscreen.dart
+++ lib/src/views/authentications/splashscreen.dart
@@ -3,6 +3,8 @@ import 'package:shared_preferences/shared_preferences.dart';
 import 'package:tridentpro/src/components/colors/default.dart';
 import 'package:tridentpro/src/controllers/2_factory_auth.dart';
 import 'package:get/get.dart';
+import 'package:tridentpro/src/controllers/home.dart';
+import 'package:tridentpro/src/service/auth_service.dart';
 import 'package:tridentpro/src/views/authentications/onboarding.dart';
 import 'package:tridentpro/src/views/mainpage.dart';
 
@@ -16,26 +18,26 @@ class Splashscreen extends StatefulWidget {
 class _SplashscreenState extends State<Splashscreen> {
 
   TwoFactoryAuth twoFactoryAuth = Get.put(TwoFactoryAuth());
+  HomeController homeController = Get.put(HomeController());
+  AuthService authService = Get.find();
 
   Future<bool> getLoggedIn() async {
     SharedPreferences preferences = await SharedPreferences.getInstance();
-    String? accessToken = preferences.getString('accessToken');
-    print(accessToken);
     return preferences.getBool('loggedIn') ?? false;
   }
 
   @override
   void initState() {
     super.initState();
-    getLoggedIn().then((loggedIn){
+    getLoggedIn().then((loggedIn) async {
       if(loggedIn){
-        twoFactoryAuth.refreshTokenizer().then((result){
-          if(result){
-            Get.offAll(() => const Mainpage());
-          }else{
-            Get.off(() => const Onboarding());
-          }
-        });
+        Map<String, dynamic> result = await authService.get("profile/info");
+        if(result['statusCode'] == 200){
+          Get.offAll(() => const Mainpage());
+        }else{
+          Get.off(() => const Onboarding());
+        }
+
       }else{
         Get.off(() => const Onboarding());
       }
