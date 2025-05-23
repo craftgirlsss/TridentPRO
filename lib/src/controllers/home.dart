579fb0b048b11e22ec6a7ee8ef91dc4fcc84abc8
Fani Annur Rozi <crazyhx4@gmail.com>
2025-05-23 16:38:22 +0700

    Create class HomeController

---
+++ lib/src/controllers/home.dart
@@ -0,0 +1,26 @@
+import 'package:get/get.dart';
+import 'package:tridentpro/src/models/auth/profile.dart';
+import 'package:tridentpro/src/service/auth_service.dart';
+
+class HomeController extends GetxController {
+  AuthService authService = AuthService();
+  Rxn<ProfileModel> profileModel = Rxn<ProfileModel>();
+
+  Future<bool> profile() async {
+    try {
+      Map<String, dynamic> response = await authService.get("profile/info");
+      if(response['success'] != true) {
+        // return false;
+      }
+
+      /** Coba access token disalahkan agar terdeteksi token expired */
+      // authService.accessToken = "abogoboga";
+
+      profileModel(ProfileModel.fromJson(response['response']));
+      return response['success'] ?? false;
+
+    } catch (e) {
+      throw e;
+    }
+  }
+}
