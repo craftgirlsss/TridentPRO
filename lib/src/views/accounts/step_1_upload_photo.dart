053aad10ab26d090f7683e177d749f2b0a07293b
Fani Annur Rozi <crazyhx4@gmail.com>
2025-05-23 16:44:16 +0700

    edit: test multipart

--- lib/src/views/accounts/step_1_upload_photo.dart
+++ lib/src/views/accounts/step_1_upload_photo.dart
@@ -6,7 +6,9 @@ import 'package:tridentpro/src/components/appbars/default.dart';
 import 'package:tridentpro/src/components/bottomsheets/material_bottom_sheets.dart';
 import 'package:tridentpro/src/components/containers/utilities.dart';
 import 'package:tridentpro/src/components/languages/language_variable.dart';
+import 'package:tridentpro/src/components/textfields/phone_textfield.dart';
 import 'package:tridentpro/src/components/textfields/void_textfield.dart';
+import 'package:tridentpro/src/controllers/regol.dart';
 import 'package:tridentpro/src/helpers/handlers/image_picker.dart';
 import 'package:tridentpro/src/helpers/variables/countries.dart';
 import 'package:tridentpro/src/helpers/variables/id_type.dart' show idTypeList;
@@ -25,14 +27,18 @@ class _Step1UploadPhotoState extends State<Step1UploadPhoto> {
   final _formKey = GlobalKey<FormState>();
   TextEditingController nationallyController = TextEditingController();
   TextEditingController idTypeController = TextEditingController();
+  TextEditingController idTypeNumber = TextEditingController();
+  RegolController regolController = Get.put(RegolController());
 
   RxString idPhoto = "".obs;
   RxString idPhotoSelfie = "".obs;
+  RxBool isLoading = false.obs;
 
   @override
   void dispose() {
     nationallyController.dispose();
     idTypeController.dispose();
+    idTypeNumber.dispose();
     super.dispose();
   }
 
@@ -61,43 +67,45 @@ class _Step1UploadPhotoState extends State<Step1UploadPhoto> {
             child: Column(
               children: [
                 UtilitiesWidget.titleContent(
-                    title: LanguageGlobalVar.TITLE_REGOL_PAGE_1.tr,
-                    subtitle: LanguageGlobalVar.SUBTITLE_REGOL_PAGE_1.tr,
-                    children: [
-                      const SizedBox(height: 10),
-                      VoidTextField(controller: nationallyController, fieldName: LanguageGlobalVar.NATIONALY.tr, hintText: LanguageGlobalVar.CHOOSE_NATIONALY.tr, labelText: LanguageGlobalVar.NATIONALY.tr, onPressed: (){
-                        CustomMaterialBottomSheets.defaultBottomSheet(context, size: size, title: LanguageGlobalVar.CHOOSE_NATIONALY.tr, children: List.generate(countryList.length, (i){
-                          return ListTile(
-                            onTap: (){
-                              Navigator.pop(context);
-                              nationallyController.text = countryList[i].name;
-                            },
-                            title: Text(countryList[i].name, style: GoogleFonts.inter()),
-                          );
-                        }));
-                      }),
-                      VoidTextField(onPressed: (){
-                        CustomMaterialBottomSheets.defaultBottomSheet(context, size: size, title: LanguageGlobalVar.CHOOSE_YOUR_ID_TYPE.tr, isScrolledController: false, children: List.generate(idTypeList.length, (i){
-                          return ListTile(
-                            onTap: (){
-                              Navigator.pop(context);
-                              idTypeController.text = idTypeList[i].name;
-                            },
-                            title: Text(idTypeList[i].name, style: GoogleFonts.inter()),
-                          );
-                        }));
-                      },
-                        controller: idTypeController, fieldName: LanguageGlobalVar.ID_TYPE.tr, hintText: LanguageGlobalVar.ID_TYPE.tr, labelText: LanguageGlobalVar.ID_TYPE.tr),
-                      Obx(
-                        () => UtilitiesWidget.uploadPhoto(title: LanguageGlobalVar.ID_CARD_PHOTO.tr, onPressed: () async {
-                          idPhoto.value = await CustomImagePicker.pickImageFromCameraAndReturnUrl();
-                        }, urlPhoto: idPhoto.value),
-                      ),
-                      Obx(
-                        () => UtilitiesWidget.uploadPhoto(title: LanguageGlobalVar.SELFIE_WITH_ID.tr, urlPhoto: idPhotoSelfie.value, onPressed: () async {
-                          idPhotoSelfie.value = await CustomImagePicker.pickImageFromCameraAndReturnUrl();
-                        })),
-                    ]
+                  title: LanguageGlobalVar.TITLE_REGOL_PAGE_1.tr,
+                  subtitle: LanguageGlobalVar.SUBTITLE_REGOL_PAGE_1.tr,
+                  children: [
+                    const SizedBox(height: 10),
+                    VoidTextField(controller: nationallyController, fieldName: LanguageGlobalVar.NATIONALY.tr, hintText: LanguageGlobalVar.CHOOSE_NATIONALY.tr, labelText: LanguageGlobalVar.NATIONALY.tr, onPressed: (){
+                      CustomMaterialBottomSheets.defaultBottomSheet(context, size: size, title: LanguageGlobalVar.CHOOSE_NATIONALY.tr, children: List.generate(countryList.length, (i){
+                        return ListTile(
+                          onTap: (){
+                            Navigator.pop(context);
+                            nationallyController.text = countryList[i].name;
+                          },
+                          title: Text(countryList[i].name, style: GoogleFonts.inter()),
+                        );
+                      }));
+                    }),
+                    VoidTextField(onPressed: (){
+                      CustomMaterialBottomSheets.defaultBottomSheet(context, size: size, title: LanguageGlobalVar.CHOOSE_YOUR_ID_TYPE.tr, isScrolledController: false, children: List.generate(idTypeList.length, (i){
+                        return ListTile(
+                          onTap: (){
+                            Navigator.pop(context);
+                            idTypeController.text = idTypeList[i].name;
+                          },
+                          title: Text(idTypeList[i].name, style: GoogleFonts.inter()),
+                        );
+                      }));
+                    },
+                      controller: idTypeController, fieldName: LanguageGlobalVar.ID_TYPE.tr, hintText: LanguageGlobalVar.ID_TYPE.tr, labelText: LanguageGlobalVar.ID_TYPE.tr
+                    ),
+                    PhoneTextField(controller: idTypeNumber, fieldName: LanguageGlobalVar.ID_TYPE_NUMBER.tr, hintText: LanguageGlobalVar.ID_TYPE_NUMBER.tr, labelText: LanguageGlobalVar.ID_TYPE_NUMBER.tr),
+                    Obx(
+                      () => isLoading.value ? const SizedBox() : UtilitiesWidget.uploadPhoto(title: "Foto KTP", onPressed: () async {
+                        idPhoto.value = await CustomImagePicker.pickImageFromCameraAndReturnUrl();
+                      }, urlPhoto: idPhoto.value),
+                    ),
+                    Obx(
+                      () => isLoading.value ? const SizedBox() : UtilitiesWidget.uploadPhoto(title: "Foto Selfie", urlPhoto: idPhotoSelfie.value, onPressed: () async {
+                        idPhotoSelfie.value = await CustomImagePicker.pickImageFromCameraAndReturnUrl();
+                      })),
+                  ]
                 ),
               ],
             ),
@@ -107,7 +115,14 @@ class _Step1UploadPhotoState extends State<Step1UploadPhoto> {
           size: size,
           title: LanguageGlobalVar.VERIFICATION_IDENTITY.tr,
           onPressed: (){
-            Get.to(() => const Step2StoredData());
+            regolController.verifikasiIdentitas(
+              country: nationallyController.text,
+              idType: idTypeController.text,
+              idTypeNumber: idTypeNumber.text,
+              appFotoIdentitas: idPhoto.value,
+              appFotoTerbaru: idPhotoSelfie.value
+            );
+            // Get.to(() => const Step2StoredData());
           },
           progressEnd: 4,
           progressStart: 1
