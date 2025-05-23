893a35ed81f22666f6a00abde663990c5dc98798
Fani Annur Rozi <crazyhx4@gmail.com>
2025-05-23 17:05:11 +0700

    fix: error wrong parameter authService.refreshingToken and profile return key status instead of success

--- test/auth_service_test.dart
+++ test/auth_service_test.dart
@@ -1,12 +1,10 @@
 import 'package:flutter_test/flutter_test.dart';
-import 'package:mockito/mockito.dart';
 import 'package:mockito/annotations.dart';
-import 'package:tridentpro/src/helpers/variables/global_variables.dart';
 import '../lib/src/service/auth_service.dart';
-import 'auth_service_test.mocks.dart';
 
 @GenerateMocks([AuthService])
 void main() {
+  
   group('AuthService Tests', () {
     test('should send correct login data', () async {
       final authService = AuthService();
@@ -30,10 +28,10 @@ void main() {
     test('should return new access & refresh token', () async {
       final authService = AuthService();
       final body = {
-        'refresh_token': 'eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJ1c2VyX2lkIjoiMTc0NzcwNjYxMCIsInR5cGUiOiJyZWZyZXNoIiwiZXhwIjoxNzUwNTYyMTYwfQ==.+jPquEmgd7pC8HbvItlaE9QqBNH6y2YKbJ1yi8/FGWg=',
+        'refresh_token': '',
       };
 
-      final result = await authService.refreshingToken("auth/refresh", body);
+      final result = await authService.refreshingToken(body: body);
 
       print(result);
       expect(result, isA<Map<String, dynamic>>());
@@ -43,13 +41,12 @@ void main() {
 
     test('should return new access & refresh token if response code 300', () async {
       final authService = AuthService();
-      await authService.init();
-
+      
       final fetchProfile = await authService.get("profile/info");
       print(fetchProfile);
 
       expect(fetchProfile, isA<Map<String, dynamic>>());
-      expect(fetchProfile.containsKey("success"), true);
+      expect(fetchProfile.containsKey("status"), true);
     });
   });
 }
