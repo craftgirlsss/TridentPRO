e79edc676d06c781066c1fe4dc6cf37c287fd492
Fani Annur Rozi <crazyhx4@gmail.com>
2025-05-23 16:45:11 +0700

    edit: automatic taking profile when the tab is opened

--- lib/src/views/settings/index.dart
+++ lib/src/views/settings/index.dart
@@ -1,5 +1,9 @@
 import 'package:flutter/material.dart';
+import 'package:get/get.dart';
+import 'package:get/get_rx/src/rx_types/rx_types.dart';
 import 'package:tridentpro/src/components/colors/default.dart';
+import 'package:tridentpro/src/controllers/home.dart';
+import 'package:tridentpro/src/models/auth/profile.dart';
 
 class Settings extends StatefulWidget {
   const Settings({super.key});
@@ -9,6 +13,19 @@ class Settings extends StatefulWidget {
 }
 
 class _SettingsState extends State<Settings> {
+  HomeController homeController = Get.find();
+
+  @override
+  void initState() {
+    super.initState();
+    homeController.profile();
+  }
+
+  @override
+  void dispose() {
+    super.dispose();
+  }
+
   @override
   Widget build(BuildContext context) {
     final Size size = MediaQuery.of(context).size;
@@ -17,7 +34,7 @@ class _SettingsState extends State<Settings> {
       width: size.width,
       height: size.height,
       child: Center(
-        child: Text("Settings"),
+        child: Obx(() => Text("Settings: ${homeController.profileModel.value?.name}")),
       ),
     );
   }
